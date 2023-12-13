d7db58fa296c1feb95d3154ef3dc77d2470d13be
Zaela Rose <zaela.rose@tutanota.com>
2023-12-13 12:58:02 -0600

    init commit

---
+++ lib/expenses.dart
@@ -0,0 +1,43 @@
+import 'package:expense_tracker/expense.dart';
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Tutanota',
+      amount: 12.00,
+      date: DateTime.now(),
+      category: Category.other,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('Chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
